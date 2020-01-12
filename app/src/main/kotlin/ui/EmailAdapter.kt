8b6d2a2050fc8e7bdbabbac7a68c4be106b2e6d8
Robin Linden <dev@robinlinden.eu>
2020-01-12 15:43:48 +0100

    Simplify open/closed email handling

--- app/src/main/kotlin/ui/EmailAdapter.kt
+++ app/src/main/kotlin/ui/EmailAdapter.kt
@@ -6,11 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
-import kotlinx.android.synthetic.main.email_list_item_open.view.*
-
+import androidx.core.text.parseAsHtml
+import kotlinx.android.synthetic.main.email_list_item.view.*
 import org.cmdline.ackr.Email
 import org.cmdline.ackr.R
-import java.util.*
 
 class EmailAdapter(private val inflater: LayoutInflater) : BaseAdapter() {
     var emails: List<Email> = listOf()
@@ -20,43 +19,40 @@ class EmailAdapter(private val inflater: LayoutInflater) : BaseAdapter() {
     override fun getItemId(position: Int): Long = position.toLong()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view: View
-        val r: Int
-        val email = emails[position]
-
-
         // FIXME this isn't how Recycling works
-        if (email.open) {
-            r = R.layout.email_list_item_open
-            view = inflater.inflate(r, parent, false)
-        } else {
-            r = R.layout.email_list_item
-            view = inflater.inflate(r, parent, false)
-        }
+        val r = R.layout.email_list_item
+        val view = inflater.inflate(r, parent, false)
 
-        val vh = ViewHolderCommon(view, email)
+        val vh = ViewHolderCommon(view)
+        val email = emails[position]
         vh.from.text = email.from
         vh.subject.text = email.subject
-        vh.body?.text = email.body
+        vh.body?.text = email.body.parseAsHtml().toString()
         vh.recv_date.text = email.recv_date.toString()
 
         view.tag = vh
 
+        view.body.visibility = if (email.open) {
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+
+        if (email.read) {
+            view.from.setTypeface(null, Typeface.NORMAL)
+            view.subject.setTypeface(null, Typeface.NORMAL)
+        } else {
+            view.from.setTypeface(null, Typeface.BOLD)
+            view.subject.setTypeface(null, Typeface.BOLD)
+        }
+
         return view
     }
 
-    private class ViewHolderCommon(view: View, email: Email) {
+    private class ViewHolderCommon(view: View) {
         val subject: TextView = view.subject
         val from: TextView = view.from
         val recv_date: TextView = view.recv_date
         val body: TextView? = view.body
-
-        init {
-            if (!email.read) {
-                from.setTypeface(null, Typeface.BOLD);
-                subject.setTypeface(null, Typeface.BOLD);
-            }
-        }
     }
-
 }
